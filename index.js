cad0a325b6d4e6f2f55711fc387f2d9fb12c93a6
Augusto Seta <alseta97@gmail.com>
2023-02-08 12:05:28 -0300

    the database is used in route controllers. HTTP requests can now GET the data from the database, POST new notes, and GET a specific note by its id.

--- index.js
+++ index.js
@@ -63,36 +63,29 @@ const genetareId = () => {
 
 app.post('/api/notes', (request, response) => {
   const body = request.body
+  console.log(body);
 
-  if (!body.content) {
+  if (body.content === undefined) {
     return response.status(404).json({
       error: 'content missing'
     })
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
-    important: body.important || false,
-    date: new Date(),
-    id: genetareId(),
-  }
+    important: body.important
+  })
 
-  notes = notes.concat(note)
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
 
-  response.json(note)
 })
 
 app.get('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const note = notes.find(note => note.id === id)
-  
-  if (note) {
+  Note.findById(request.params.id).then(note => {
     response.json(note)
-  } else {
-    response.status(404).end()
-  }
-
-  response.json(note)
+  })
 })
 
 app.delete('/api/notes/:id', (request, response) => {
