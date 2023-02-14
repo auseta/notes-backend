6bf246ddbe914276496bd9c79abf957cf7e1e1ea
Augusto Seta <alseta97@gmail.com>
2023-02-14 00:50:48 -0300

    the project has been structured to comply with the best practices of Node.js

--- models/note.js
+++ models/note.js
@@ -1,25 +1,14 @@
 const mongoose = require('mongoose')
 
-mongoose.set('strictQuery', false)
-
-const url = process.env.MONGODB_URI
-
-console.log('connecting to', url)
-
-mongoose.connect(url)
-  .then(result => {
-    console.log('connected to mongoDB')
-  })
-  .catch(error => {
-    console.log('error connecting to mongoDB:', error.message)
-  })
-
-// field validation
 const noteSchema = new mongoose.Schema({
   content: {
     type: String,
     minLength: 5,
     required: true
+  },
+  date: {
+    type: Date,
+    required: true,
   },
   important: Boolean,
 })
